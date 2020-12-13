c155f41a640bd703ba921e7880b5a1ca692ac0c7
TaiyiLin <0taiyilin0@gmail.com>
2020-12-13 21:53:48 +0800

    homeViewModel debug

--- app/src/main/java/com/taiyilin/mandarinlearning/main/home/HomeViewModel.kt
+++ app/src/main/java/com/taiyilin/mandarinlearning/main/home/HomeViewModel.kt
@@ -8,7 +8,6 @@ import com.taiyilin.mandarinlearning.MandarinLearningApplication
 import com.taiyilin.mandarinlearning.data.Classroom
 import com.taiyilin.mandarinlearning.data.Course
 import com.taiyilin.mandarinlearning.data.source.MandarinLearningRepository
-import com.taiyilin.mandarinlearning.data.source.remote.MandarinLearningRemoteDataSource.getAllCourses
 import com.taiyilin.mandarinlearning.network.LoadApiStatus
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,8 +22,6 @@ class HomeViewModel(private val repository: MandarinLearningRepository) : ViewMo
 
     val course: LiveData<List<Course>>
     get() = _course
-
-    var liveCourses = MutableLiveData<List<Course>>()
 
     var liveCourses = MutableLiveData<List<Course>>()
 
